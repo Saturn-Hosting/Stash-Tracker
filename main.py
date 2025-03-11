287dcd7a059304d06a1e663a7be9313737e1e325
Fulgur <fulgurgithub@gmail.com>
2025-03-11 17:30:33 +0100

    Migrations

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
-import mysql.connector
+import sqlite3
 
-db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="",
+conn = sqlite3.connect('stash_tracker.db')
+
+cursor = conn.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS stash (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    location TEXT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
 )
+''')
 
-cursor = db.cursor()
-cursor.execute("CREATE DATABASE IF NOT EXISTS stash_tracker")
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS kit (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    stash_id INTEGER NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (stash_id) REFERENCES stash (id)
+)
+''')
 
-print(db)
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS item (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    kit_id INTEGER NOT NULL,
+    count INTEGER NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (kit_id) REFERENCES kit (id)
+)
+''')
+
+conn.commit()
+conn.close()
