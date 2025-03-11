5322c9b928b1a16fc77c8fc266852cc3840a371e
Fulgur <fulgurgithub@gmail.com>
2025-03-11 18:57:27 +0100

    Bugfix: same thread error

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app.add_middleware(
 )
 
 def get_db():
-    conn = sqlite3.connect("stash_tracker.db")
+    conn = sqlite3.connect("stash_tracker.db", check_same_thread=False)
     conn.row_factory = sqlite3.Row
     return conn
 
